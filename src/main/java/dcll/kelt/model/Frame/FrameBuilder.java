818e67d32fa791af05e0ba713a1bb6580b334166
Julian <julian.bironneau@hotmail.fr>
2015-03-31 23:55:45 +0200

    #11 Correcting FrameBuilder.java for checkstyle. Remaining errors relative to throws exception which can t be fixed now (see problems concerning this rule on the web).

--- src/main/java/dcll/kelt/model/Frame/FrameBuilder.java
+++ src/main/java/dcll/kelt/model/Frame/FrameBuilder.java
@@ -8,39 +8,66 @@ import java.util.Queue;
  * Created by kelto on 31/03/15.
  */
 public class FrameBuilder {
-    private static final char SEPARATOR = '-';
-    private Queue<Character> queue;
 
-    public FrameBuilder(Queue<Character> queue) {
-        this.queue = queue;
+    /**
+     * Class variable SEPARATOR which separates the frame.
+     */
+    private static final char SEPARATOR = '-';
+
+    /**
+     * A queue of characters representing the whole game.
+     */
+    private final Queue<Character> queue;
+
+    /**
+     * Constructor of FrameBuilder.
+     * @param uneQueue
+     */
+    public FrameBuilder(final Queue<Character> uneQueue) {
+        this.queue = uneQueue;
     }
 
-    public Deque<Frame> getFrames() throws Exception {
+    /**
+     * Method to get a frame of the game.
+     * @return deque
+     * @throws Exception
+     */
+    public final Deque<Frame> getFrames() throws Exception {
         Deque<Frame> deque = new ArrayDeque<Frame>();
-        while(!queue.isEmpty()) {
+        while (!queue.isEmpty()) {
             deque.push(createFrame());
         }
         return deque;
     }
 
+    /**
+     * Method to create a frame with the queue of characters.
+     *
+     * @return frame
+     * @throws Exception
+     */
     private Frame createFrame() throws Exception {
-        if(queue.size()<3) {
+
+        final int topFrame = 3;
+        if (queue.size() < topFrame) {
             throw new Exception("Not enough char to create a Frame.");
         }
-        char first,second,separator;
+        char first, second, separator;
         Frame frame;
         first = queue.poll();
         second = queue.poll();
         separator = queue.poll();
-        if(separator != SEPARATOR) {
-            throw new Exception("Invalid Syntax: Separator for frame should be '-'.");
+        if (separator != SEPARATOR) {
+            throw new
+                    Exception("Invalid Syntax: "
+                    + "Separator for frame should be '-'.");
         }
-        if(first == Frame.STRIKE) {
-            frame = new StrikeFrame(first,second);
-        } else if(second == Frame.SPARE) {
-            frame = new SpareFrame(first,second);
+        if (first == Frame.STRIKE) {
+            frame = new StrikeFrame(first, second);
+        } else if (second == Frame.SPARE) {
+            frame = new SpareFrame(first, second);
         } else {
-            frame = new NormalFrame(first,second);
+            frame = new NormalFrame(first, second);
         }
         return frame;
     }
